3cbfba2db158216ebfd2c16fc2ee1160fa3f38a4
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2023-01-24 11:21:01 +0400

    data types example

--- 01-Exercises/01-IntroAndTypes/01-helloWorld.dart
+++ 01-Exercises/01-IntroAndTypes/01-helloWorld.dart
@@ -1,3 +1,6 @@
 void main(List<String> args) {
   print("hello world");
+  print("hello Gurban");
+  print("3+5");
+  print(3 + 5);
 }
---
+++ 01-Exercises/01-IntroAndTypes/02-dataTypes.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  int num1 = 5;
+  // int num1 = 5.5;
+  double num2 = 5.5;
+
+  num il = 2022;
+
+  String name = "Gurban";
+  String surName = "Gurbanzada";
+
+  bool man = true;
+
+  var school = "code academy";
+  var school1 = true;
+  var school2 = 17;
+
+  print(num1);
+  print(num2);
+  print(il);
+  print(name);
+  print(name + " " + surName);
+  print(man);
+  print(school);
+  print(school1);
+  print(school2);
+}
