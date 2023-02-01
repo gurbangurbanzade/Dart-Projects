8c634f00a237845a414901a437b015711a12954b
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2023-02-01 22:56:28 +0400

    class example

---
+++ 01-Exercises/05-OOP/01-Class.dart
@@ -0,0 +1,19 @@
+import 'dart:ffi';
+
+void main(List<String> args) {
+  Student gurban = Student();
+  gurban.name = "Gurbanzada";
+  gurban.age = 29;
+  gurban.grad = true;
+  print(gurban);
+}
+
+class Student {
+  int age = 0;
+  String name = "";
+  bool grad = false;
+
+  void sayHello() {
+    print("hello world");
+  }
+}
